db2e79c29dec772d3071c9bb37b49b4c85aa032a
Vasilev07 <georgi_vasilev07@abv.bg>
2018-03-21 18:48:53 +0200

    Added validations for password, username, email

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -3,12 +3,40 @@ class UserController {
         this.data = data;
     }
 
-    validateUserEmail() {}
-    validateUsername() {}
-    validatePasswords() {}
+    validateUserEmail(usersEmailArray, currentUserEmail) {
+        const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        const validateEmail = pattern.test(currentUserEmail);
 
-    createUser(user) {
+        if (usersEmailArray.includes(currentUserEmail)) {
+            throw new Error('This email already exists');
+        } else if (validateEmail === false) {
+            throw new Error('This is not valid email');
+        } else if (currentUserEmail === '') {
+            throw new Error('Email must not be empty');
+        } else {
+            return currentUserEmail;
+        }
     }
+    validateUsername(usernameArray, currentUsername) {
+        if (usernameArray.includes(currentUsername)) {
+            throw new Error('This username already exists');
+        } else if (usernameArray === '') {
+            throw new Error('Username must not be empty');
+        } else {
+            return currentUsername;
+        }
+    }
+    validatePasswords(password, repassword) {
+        if (password !== repassword) {
+            throw new Error('Password do not match');
+        } else if (password.length < 4) {
+            throw new Error('Password must be atleast 5 symbols');
+        } else {
+            return password;
+        }
+    }
+
+    createUser(user) {}
 }
 
 module.exports = UserController;
