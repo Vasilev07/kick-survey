70bc8a0b9a68f54edfa7cf1ad569d5251edd447e
Vasilev07 <georgi_vasilev07@abv.bg>
2018-03-27 16:34:57 +0300

    adde some Mixins and create questions jquery logics

--- public/js/create-survey.js
+++ public/js/create-survey.js
@@ -1,20 +1,33 @@
 /* eslint-disable */
 
 $(function () {
+    $('#add-question-btn').hide();
     $('#continue-btn').click(function (e) {
         e.preventDefault();
         var surveyData = {
             surveyName: $('#survey-name').val(),
             category: $('#categories').val(),
         };
-        console.log(surveyData);
+        var questionData = {
+            questionTypes: $('.question-types').children(),
+        }
+        var questionTypesHolder = [];
+        $.each(questionData.questionTypes, function (key, value) {
+            questionTypesHolder.push($(value).attr('value'));
+        });
         $('#initial-create').hide();
 
         $('p.survey-name').text(surveyData.surveyName);
         $('p.category').text(surveyData.category);
 
         $('#create-survey-form').show();
+        $('#add-question-btn').show();
         // AJAX request here
+    });
+    $('#add-question-btn').click(function (e) {
+        e.preventDefault();
+        var create = $('#create-survey-form');
+        $('#wrapper').append(create.html());
     });
 });
 
